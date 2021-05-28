ed9e51ebb77bd8497e4fb8611a488931d074db44
wangze <zacharywong880@outlook.com>
2021-05-28 11:56:47 +0800

    练习

--- 2021_04_09/src/com/MathTest.java
+++ 2021_04_09/src/com/MathTest.java
@@ -3,6 +3,9 @@ package com;
 //Math 类练习
 public class MathTest {
     public static void main(String[] args) {
+        // 生成一个double类型的随机数，随机数范围为[0,1)
         System.out.println(Math.random());
+        //取绝对值
+        System.out.println(Math.abs(-20));
     }
 }
