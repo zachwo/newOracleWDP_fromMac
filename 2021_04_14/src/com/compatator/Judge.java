39c856e8b377db9c6c4f37ff88b01806b02484d0
wangze <zacharywong880@outlook.com>
2021-06-04 11:12:16 +0800

    练习

--- 2021_04_14/src/com/compatator/Judge.java
+++ 2021_04_14/src/com/compatator/Judge.java
@@ -3,6 +3,7 @@ import java.util.Comparator;
 //相较于让Student类implements Comparable接口并重写compareTo方法，
 //让Judge类implements Comparator<NewStudent>接口这种方法可以实现非侵入式添加新功能
 //非侵入思想：在不改变原有代码的基础上利用其特点实现额外的功能
+//降低了耦合度
 //MVC
 public class Judge implements Comparator<NewStudent> {
     @Override
